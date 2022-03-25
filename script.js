5da9b01faafc739a9f084a374b11b073e956ad1f
JCT000 <juliustindugan14@gmail.com>
2022-03-26 00:03:53 +0800

    handson7

---
+++ script.js
@@ -0,0 +1,27 @@
+function handleClick(element) {
+  element.style = "background-color:#5DADE2;"
+}
+var image = document.getElementById("image");
+image.addEventListener("mouseover", function(){
+  this.style ="box-shadow:2px 2px 2px grey";
+  this.width ="150"
+});
+
+image.addEventListener("mouseout", function(){
+  this.style ="";
+  this.width ="130"
+});
+
+function handleClick(element) {
+  element.style = "background-color:#5DADE2;"
+}
+var image = document.getElementById("img");
+image.addEventListener("mouseover", function(){
+  this.style ="box-shadow:2px 2px 2px grey";
+  this.width ="150"
+});
+
+image.addEventListener("mouseout", function(){
+  this.style ="";
+  this.width ="130"
+});
